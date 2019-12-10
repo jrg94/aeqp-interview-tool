0b3ebc96d1b4842137e3477d0326949ecb68d6e7
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-12-10 16:34:07 -0500

    Toying with the audio library

--- audio_manager.py
+++ audio_manager.py
@@ -1,5 +1,24 @@
-import pyaudio # Needed Visual C++ 14.0 for this
+import pyaudio  # Needed Visual C++ 14.0 for this
+import wave
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 5
+
 
 class AudioManager:
     def __init__(self):
-        pass
+        self.audio = pyaudio.PyAudio()
+        self.stream = None
+
+    def start(self):
+        self.stream = self.audio.open(format=FORMAT, channels=CHANNELS,
+                                      rate=RATE, input=True,
+                                      frames_per_buffer=CHUNK)
+
+    def stop(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
