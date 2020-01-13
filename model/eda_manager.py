7d87c5aa7267fb501ed2527f4183a6b1ba541c4a
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-01-13 11:27:26 -0500

    Added a device connect command
    And, several constants

--- model/eda_manager.py
+++ model/eda_manager.py
@@ -5,8 +5,26 @@ class EDAManager:
 
     LOCALHOST = "127.0.0.1"
     PORT = 28000
+
+    # Empatica E4 Commands
     LIST_DEVICES_COMMAND = "device_list"
     CONNECT_DEVICE_COMMAND = "device_connect"
+    DISCONNECT_DEVICE_COMMAND = "device_disconnect"
+    STREAM_SUBSCRIBE_COMMAND = "device_subscribe"
+
+    # Empatica E4 Streams
+    THREE_AXIS_ACCELERATION = "acc"
+    BLOOD_VOLUME_PULSE = "bvp"
+    GALVANIC_SKIN_RESPONSE = "gsr"
+    INTERBEAT_INTERVAL_AND_HEARTRATE = "ibi"
+    SKIN_TEMPERATURE = "tmp"
+    DEVICE_BATTERY = "bat"
+    DEVICE_TAG = "tag"
+
+    # Empatica E4 Status Codes
+    STATUS_CODE_ERR = "ERR"
+    STATUS_CODE_OK = "OK"
+
     COMMAND_SEPARATOR = "|"
 
     def __init__(self):
@@ -30,10 +48,16 @@ class EDAManager:
         """
         device_list_command = EDAManager._construct_command(EDAManager.LIST_DEVICES_COMMAND)
         self.socket.sendall(device_list_command)
-        command = self.socket.recv(1024).decode("utf-8")
-        devices = list(map(str.strip, command.split(EDAManager.COMMAND_SEPARATOR)[1:]))
+        response = self.socket.recv(1024).decode("utf-8")
+        devices = list(map(str.strip, response.split(EDAManager.COMMAND_SEPARATOR)[1:]))
         return devices
 
+    def _connect_device(self, device_id) -> bool:
+        device_connect_command = EDAManager._construct_command(EDAManager.CONNECT_DEVICE_COMMAND, device_id)
+        self.socket.sendall(device_connect_command)
+        response = self.socket.recv(1024).decode("utf-8")
+        status_code = response.split(" ")[2]
+        return True if status_code == EDAManager.STATUS_CODE_OK else False
 
 manager = EDAManager()
 manager.start_recording()
