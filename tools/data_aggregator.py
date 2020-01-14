4e946bfcfbcbe8611f5c0ba3970f4270d034c4c0
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-01-13 22:45:00 -0500

    Optimized imports

--- tools/data_aggregator.py
+++ tools/data_aggregator.py
@@ -1,7 +1,7 @@
-import tkinter as tk
-from tkinter import filedialog
 import csv
+import tkinter as tk
 from contextlib import ExitStack
+from tkinter import filedialog
 
 root = tk.Tk()
 root.withdraw()
@@ -9,6 +9,6 @@ root.withdraw()
 file_paths = filedialog.askopenfilenames()
 
 with ExitStack() as stack:
-    files = [stack.enter_context(open(fname)) for fname in file_paths]
+    files = [stack.enter_context(open(path)) for path in file_paths]
     for file in files:
         csv_mapping_list = list(csv.DictReader(file))
