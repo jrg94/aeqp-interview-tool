f7d3a94d1e3a04a6a95bbe31b5ddf6eed0317d40
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-01-13 22:38:55 -0500

    Added file reading

--- tools/data_aggregator.py
+++ tools/data_aggregator.py
@@ -1,7 +1,14 @@
 import tkinter as tk
 from tkinter import filedialog
+import csv
+from contextlib import ExitStack
 
 root = tk.Tk()
 root.withdraw()
 
 file_paths = filedialog.askopenfilenames()
+
+with ExitStack() as stack:
+    files = [stack.enter_context(open(fname)) for fname in file_paths]
+    for file in files:
+        csv_mapping_list = list(csv.DictReader(file))
